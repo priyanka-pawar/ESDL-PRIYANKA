e4e8a1cd65cf4bc29a8a09fda5675da7839a857f
priyanka-pawar <priyanka.pawar2@cumminscollege.in>
2014-07-30 14:40:49 +0530

    Update calc.cpp
    new method subtraction is added..

--- calc.cpp
+++ calc.cpp
@@ -4,12 +4,19 @@ void main()
   
 cout<<"Welcome to GitHub";
 cout<<square(1.0);
-
+cout<<"subtraction"<<sub(2.0)
 }
 
 double square(double in_num)
 {
   double out_num;
   out_num=in_num+in_num;
+  return out_num;
+}
+
+double sub(double in_num)
+{
+  double out_num;
+  out_num=in_num-in_num;
   return out_num;
 }
