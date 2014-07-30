015e89653a931ae33e02ec828a0fab9a415e2664
priyanka-pawar <priyanka.pawar2@cumminscollege.in>
2014-07-30 14:30:42 +0530

    Update calc.cpp
    square method added..

--- calc.cpp
+++ calc.cpp
@@ -1,5 +1,15 @@
 #include<iostream.h>
 void main()
 {
+  
 cout<<"Welcome to GitHub";
+cout<<square(1.0);
+
+}
+
+double square(double in_num)
+{
+  double out_num;
+  out_num=in_num+in_num;
+  return out_num;
 }
